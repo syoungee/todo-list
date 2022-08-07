103e3bacdf75927f689c1ac0e2d6d951d5346877
syoungee <sunyoungah69@gmail.com>
2022-08-07 09:53:13 +0900

    reafactor: 라우트 경로 수정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Main from "./components/Main";
 import Todo from "./components/Todo";
-import Welcome from "./components/Welcome";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate replace to="/auth" />} />
         <Route path="/auth" element={<Main />} />
-        <Route path="/login" element={<Main isShow="login" />} />
         <Route path="/todo" element={<Todo />} />
       </Routes>
     </BrowserRouter>
